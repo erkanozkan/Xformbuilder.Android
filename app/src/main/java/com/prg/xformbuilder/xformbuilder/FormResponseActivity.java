06e4e3b956a0d88174bf5963c8a6e65bdfa50e19
erkanozkan <erkan@pragmalinq.com>
2015-04-10 10:49:10 +0300

    file upload için gerekli kodlar yazıldı

--- app/src/main/java/com/prg/xformbuilder/xformbuilder/FormResponseActivity.java
+++ app/src/main/java/com/prg/xformbuilder/xformbuilder/FormResponseActivity.java
@@ -1,15 +1,41 @@
 package com.prg.xformbuilder.xformbuilder;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.Parcelable;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.ConsoleMessage;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import java.io.File;
 
 
 public class FormResponseActivity extends ActionBarActivity {
     DatabaseHandler dbHandler;
     String formId="";
+
+    private WebView webView;
+
+    final Activity activity = this;
+
+    public Uri imageUri;
+
+    private static final int FILECHOOSER_RESULTCODE   = 2888;
+    private ValueCallback<Uri> mUploadMessage;
+    private Uri mCapturedImageURI = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,16 +47,163 @@ public class FormResponseActivity extends ActionBarActivity {
       Form form=  dbHandler.GetFormByFormId(formId);
 
 
-        WebView webview = (WebView) findViewById(R.id.webview);
-        webview.getSettings().setJavaScriptEnabled(true);
+        webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
 
+        //webView.getSettings().setUseWideViewPort(true);
+
+        //Other webview settings
+        webView.setScrollbarFadingEnabled(false);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setPluginState(WebSettings.PluginState.ON);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setSupportZoom(true);
 
         StringBuilder html = new StringBuilder();
         html.append(form.getMobileHtml());
 
 
-       webview.loadDataWithBaseURL("file:///android_asset/", html.toString(), "text/html", "utf-8", null);
+        webView.loadDataWithBaseURL("file:///android_asset/", html.toString(), "text/html", "utf-8", null);
         // webview.loadData(html.toString(),"text/html",null);
+
+        startWebView();
+    }
+
+    private void startWebView() {
+
+
+
+        //Create new webview Client to show progress dialog
+        //Called When opening a url or click on link
+
+        webView.setWebViewClient(new WebViewClient() {
+            ProgressDialog progressDialog;
+
+
+            // Called when all page resources loaded
+            public void onPageFinished(WebView view, String url) {
+
+                try{
+                    // Close progressDialog
+                    if (progressDialog.isShowing()) {
+                        progressDialog.dismiss();
+                        progressDialog = null;
+                    }
+                }catch(Exception exception){
+                    exception.printStackTrace();
+                }
+            }
+
+        });
+
+
+
+        // implement WebChromeClient inner class
+        // we will define openFileChooser for select file from camera
+        webView.setWebChromeClient(new WebChromeClient() {
+
+            // openFileChooser for Android 3.0+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType){
+                /**updated, out of the IF **/
+                mUploadMessage = uploadMsg;
+                /**updated, out of the IF **/
+
+
+
+                try{
+                    File imageStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "AndroidExampleFolder");
+                    if (!imageStorageDir.exists()) {
+                        imageStorageDir.mkdirs();
+                    }
+                    File file = new File(imageStorageDir + File.separator + "IMG_" + String.valueOf(System.currentTimeMillis()) + ".jpg");
+                    mCapturedImageURI = Uri.fromFile(file); // save to the private variable
+
+                    final Intent captureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mCapturedImageURI);
+                    // captureIntent.putExtra(MediaStore.EXTRA_SCREEN_ORIENTATION, ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+                    Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                    i.addCategory(Intent.CATEGORY_OPENABLE);
+                    i.setType("image/*");
+
+                    Intent chooserIntent = Intent.createChooser(i, "Image Chooser");
+                    chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Parcelable[] { captureIntent });
+
+                    startActivityForResult(chooserIntent, FILECHOOSER_RESULTCODE);
+                }
+                catch(Exception e){
+                    Toast.makeText(getBaseContext(), "Camera Exception:" + e, Toast.LENGTH_LONG).show();
+                }
+                //}
+            }
+
+            // openFileChooser for Android < 3.0
+            public void openFileChooser(ValueCallback<Uri> uploadMsg){
+                openFileChooser(uploadMsg, "");
+            }
+
+            //openFileChooser for other Android versions
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                openFileChooser(uploadMsg, acceptType);
+            }
+
+
+
+        });
+
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent intent) {
+
+        if(requestCode==FILECHOOSER_RESULTCODE)
+        {
+
+            if (null == this.mUploadMessage) {
+                return;
+            }
+
+            Uri result=null;
+
+            try{
+                if (resultCode != RESULT_OK) {
+
+                    result = null;
+
+                } else {
+
+                    // retrieve from the private variable if the intent is null
+                    result = intent == null ? mCapturedImageURI : intent.getData();
+                }
+            }
+            catch(Exception e)
+            {
+                Toast.makeText(getApplicationContext(), "activity :"+e, Toast.LENGTH_LONG).show();
+            }
+
+            mUploadMessage.onReceiveValue(result);
+            mUploadMessage = null;
+
+        }
+
+    }
+
+    // Open previous opened link from history on webview when back button pressed
+
+    @Override
+    // Detect when the back button is pressed
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            // Let the system handle the back button
+            super.onBackPressed();
+        }
     }
 
     @Override
