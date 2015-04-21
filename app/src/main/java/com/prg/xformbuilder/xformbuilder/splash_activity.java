dd58e327af0e19be55b2c7fb0d6b7a0dabcc6c63
Kadir <kadiro@pragmalinq.com>
2015-04-21 17:07:33 +0300

    Form List Sayfası Düzenlendi Splash Screen İçin Taslak Oluşturuldu.

--- app/src/main/java/com/prg/xformbuilder/xformbuilder/splash_activity.java
+++ app/src/main/java/com/prg/xformbuilder/xformbuilder/splash_activity.java
@@ -34,10 +34,10 @@ public class splash_activity extends Activity implements BaseSliderView.OnSlider
         mDemoSlider = (SliderLayout)findViewById(R.id.slider);
 
         HashMap<String,Integer> file_maps = new HashMap<String, Integer>();
-        file_maps.put("Hannibal",R.drawable.hannibal);
-        file_maps.put("Big Bang Theory",R.drawable.bigbang);
-        file_maps.put("House of Cards",R.drawable.house);
-        file_maps.put("Game of Thrones", R.drawable.game_of_thrones);
+        file_maps.put("Hannibal",R.drawable.splash_screen2);
+        file_maps.put("Big Bang Theory",R.drawable.preview);
+        file_maps.put("House of Cards",R.drawable.game_of_thrones);
+        file_maps.put("Game of Thrones", R.drawable.splash_screen1);
 
         for(String name : file_maps.keySet()){
             TextSliderView textSliderView = new TextSliderView(this);
